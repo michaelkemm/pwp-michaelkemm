71c83df13ef3dbffdba3830e118b9ccf00922d26
Michael Kemm <mkemm1@cnm.edu>
2016-01-19 20:54:39 -0700

    Added content to pwp-michaelkemm

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Michael Kemm's Home Page</title>
+	</head>
+	<body>
+
+		<h1>Michael Kemm</h1>
+		<h2>Purpose</h2>
+		<p>The main goal of my PWP is to showcase my web development skills.</p>
+		<h2>Audience</h2>
+		<p>My PWP will be viewed by peers, developers and potential employers</p>
+		<h2>Goal</h2>
+		<p> The main goal of my PWP is to showcase and promote my work</p>
+		<h2>Persona</h2>
+		<p> Todd: A 40-year-old small business owner that wants to establish a web presence. Todd's goal is to determine
+		whether or not to contact me about designing his website.</p>
+	</body>
+</html>
